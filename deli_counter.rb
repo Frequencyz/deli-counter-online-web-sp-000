c0d0d35aba3132402332b2de078bf4c1ecd7224c
Andrew Bay <daddnbay@yahoo.com>
2019-04-25 16:14:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,10 +19,6 @@ def take_a_number(line, name)
   return name, position 
 end 
 
-take_a_number(katz_deli, "Ada")
-take_a_number(katz_deli, "Grace")
-take_a_number(katz_deli, "Kent")
-
 def now_serving(person)
   if person.empty? == true 
     puts "There is nobody waiting to be served!"
